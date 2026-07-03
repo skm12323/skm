218f13477a74b3998efa83da793caffed99ba56e
skm <m17737996429@163.com>
2026-07-03 10:30:50 +0800

    change rotation direction

--- 预习题目/2/2_1.py
+++ 预习题目/2/2_1.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-theta = np.pi
+theta = np.pi/4 #旋转角度（逆时针）
 
 R = np.array([
-    [np.cos(theta), np.sin(theta)],
-    [-np.sin(theta),np.cos(theta)]
+    [np.cos(theta), - np.sin(theta)],
+    [np.sin(theta),np.cos(theta)]
 ])
 
 v = np.array([1., 0.])
