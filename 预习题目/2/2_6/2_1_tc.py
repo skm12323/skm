0cdc68a96c5b78ed213cc4631183a68b89955264
skm <m17737996429@163.com>
2026-07-06 16:29:35 +0800

    add 2_6

---
+++ 预习题目/2/2_6/2_1_tc.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+theta = np.pi/4 #旋转角度（顺时针）
+
+R = np.array([
+    [np.cos(theta),np.sin(theta)],
+    [-np.sin(theta),np.cos(theta)]
+])
+
+v = np.array([1., 0.])
+
+v_1 = R @ v
+
+print(v_1)
